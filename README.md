852c0e97b2db0cc182ca2991c171e0aee3ed242b
JordanCardines <61532270+jordan161727@users.noreply.github.com>
2021-04-13 14:25:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FHNITS
+
+Boothstrap and css 
+Learn From Youtube
+
+
+
+#first project did not Implements
---
+++ index.php
@@ -0,0 +1,295 @@
+<?php include('header.html');
+?>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+          <img src="img/slider/cabling1.jpg">
+          <div class="contentinslider">
+            <h2>FHNITS SOLUTIONS FOR YOU</h2>
+            <h4>highly experienced</h4>
+            <p>FHNITS are well Experienced in ALL Auxiliary Systems, Electrical Systems and Other Related Services with QUALITY WORKMANSHIP AT ALL TIMES..</p>
+         <a class="btn btn-light  btn-x1 js-scroll-trigger"  href="#services">Get Started!</a>
+          </div>
+        </div>
+        <div class="item">
+          <img src="img/slider/computer.jpg">
+          <div class="contentinslider">
+            <h2>CREATIVE DESIGN</h2>
+            <h4>Web Designer & Web Developer</h4>
+            <p>I'm Creative Designer And I Make Things Work.<br>
+            Turning Your Dreams Into Reality</p>
+       <a class="btn btn-light  btn-x1 js-scroll-trigger"  href="#services">Get Started!</a>
+          </div>
+        </div>
+       
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section bg-primary" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-10 text-center">
+          <h1 class="text-white mt-0">We've got what you need!</h1>
+          <hr class="divider light my-5">
+      <h5 class="p-first text-white"><b>FHNITS</b>  - Ferdinand Herranco Nunez InformationTechnology Solutions, are 100% Filipino owned company that are established on October 22, 2019, a DTI Registered and was owned and operated by Ferdinand Nunez, as a Sole Proprietor of the Company.FHNITS are well Experienced in ALL Auxiliary Systems, Electrical Systems and Other Related Services with QUALITY WORKMANSHIP AT ALL TIMES.<br>
+                     
+                     <b>HISTORY</b>  -  The Sole Proprietor are then Experienced in Structured Cabling System and once employee of the Reliable System Integrator, named One OPSIS Inc.
+                     Decided to build a company of his own to have more focus on the field he is questing for and to put more full attention with Quick Responses to the Solutions he is providing to every project he are dealing with.
+                     A Sole Proprietor is also Experienced Technically in SCS Designs and in Managing the Project as well, and is also excellent when it comes in Consultations regarding Structured Cabling System and Minor Electrical Works.</h5>
+   
+ 
+
+
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="WeOffer">
+    <div class="container">
+      <h2 class="text-center mt-0">What are we Offer</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa fa-globe text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Structured Cabling System</h3>
+            <p class="text-muted mb-0">Voice and Data, Ap, CCTV</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-fire text-primary mb-4"></i>
+            <h3 class="h4 mb-2">FDAS System</h3>
+            <p class="text-muted mb-0">Fire Alarm Systems</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-credit-card text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Access Control Systems</h3>
+            <p class="text-muted mb-0">Security </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Project Management and Consultations</h3>
+            <p class="text-muted mb-0">Provide Expert Advice and Solutions</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-laptop text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Web Development</h3>
+            <p class="text-muted mb-0">Personal/Bussiness Website</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 class="h4 mb-2"></h3>
+            <p class="text-muted mb-0">And Minor Electrical Works related of the above mentioned systems for complete setup components.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-desktop text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Software Development</h3>
+            <p class="text-muted mb-0">Bussiness System</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+ 
+  <!-- Portfolio Section -->
+  <section  id="portfolio" class="accomplishment" >
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/wyr.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/wyr.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+              Vitro Makati
+              </div>
+              <div class="project-name">
+              Alibaba Project(Fiber Runner and Wyr Grid Installation)
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/fiber.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/fiber.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Vitro Pasig
+              </div>
+              <div class="project-name">
+             Microsoft Project (Fiber Runner Installation)
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Meralco Multi Sites Project
+              </div>
+              <div class="project-name">
+            AP Installations
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/onlineedu.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/onlineedu.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Online Learning Lesson System
+              </div>
+              <div class="project-name">
+                        Thesis Project
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/webdev.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/webdev.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                FHNITS
+              </div>
+              <div class="project-name">
+              Website Development Project
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/bio.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/bio.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Compassion
+              </div>
+              <div class="project-name">
+                Biometrics
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="page-section" id="Contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Let's Get In Touch!</h2>
+          <hr class="divider my-5">
+          <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 ml-auto text-center">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>09773498298</div>
+        </div>
+        <div class="col-lg-2 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <a class="d-block">nunez5706@gmail.com </a>
+        </div>
+         <div class="col-lg-2 mr-auto text-center">
+          <i class="fab fa-facebook-f fa-3x mb-3 text-muted"></i>
+          <a class="d-block">Ferdinand Nuñez</a>
+        </div>
+         <div class="col-lg-2 mr-auto text-center">
+        <i class="fab fa-twitter fa-3x mb-3 text-muted"></i>
+          <a class="d-block">@ferdinandnunez</a>
+        </div>
+        <div class="col-lg-3 mr-auto text-center">
+        <i class="fab fa-instagram fa-3x mb-3 text-muted"></i>
+          <a class="d-block">@ferdinandnunez</a>
+        </div>
+      </div>
+                 
+                
+        
+
+          </div>
+    </div>
+  </section>
+
+   <!--testimonials-->
+<section class="testimonials">
+   <div class="container">
+     <h1>Testimonials</h1>
+     <p class="text-center">Our experiece share to you future clients</p>
+      <div class="row">
+        <div class="col-md-5 text-center">
+           <div class="profile">
+             <img src="img/profile/3.jpg" class="user"><br>
+               <blackquote>
+                Ferdinand Herranco Nunez InformationTechnology Solutions, are 100% Filipino owned company that are established on October 22, 2019, a DTI Registered and was owned and operated by Ferdinand Nunez, as a Sole Proprietor of the Company.FHNITS are well Experienced in ALL Auxiliary Systems, Electrical Systems and Other Related Services with QUALITY WORKMANSHIP AT ALL TIMES.
+               </blackquote> 
+             <h3> Ferdinand Nuñez    <span>Founder/CEO Founder at FHNITS</span></h3>
+           </div> 
+         </div>
+          <div class="col-md-5 text-center">
+           <div class="profile">
+             <img src="img/profile/2.jpg" class="user"><br>
+               <blackquote>
+                 I'm Jordan Cardines Freelance Aiming to become Fullstack developer. Tech Stack - Bootstrap, Vue And laravel  
+               </blackquote> 
+             <h3>Jordan Cardines <span>Web Developer</span></h3>
+           </div> 
+         </div>
+     </div>
+   </div>
+
+<style>
+    .profile h3 {
+     font-family: tahomas;
+}
+
+
+</style>
+</section>
+
+  <!-- Footer -->
+  <footer class="bg-dark py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy; 2019 - Start Bootstrap</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="asset/jquery/jquery.min.js"></script>
+  <script src="asset/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="asset/jquery-easing/jquery.easing.min.js"></script>
+  <script src="asset/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/owl.carousel.js"></script>
+  <script src="js/creative.js"></script>
+
+
+</body>
+
+</html>
